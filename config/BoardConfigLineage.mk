a0d69d5bd2dbd38da056c84541936461f1e5ae50
Frank Eggen <eggen@emden.net>
2018-03-25 14:43:04 +0200

    Revert "Fix qcom hw crypto"
    This reverts commit 72eafbbccb67359116f536fd1256ec54473b6bd3.

--- config/BoardConfigLineage.mk
+++ config/BoardConfigLineage.mk
@@ -3,7 +3,7 @@ ifeq ($(WITH_LINEAGE_CHARGER),true)
     BOARD_HAL_STATIC_LIBRARIES := libhealthd.lineage
 endif
 
-## QCOM HW crypto
-#ifeq ($(TARGET_HW_DISK_ENCRYPTION),true)
-#    TARGET_CRYPTFS_HW_PATH ?= vendor/qcom/opensource/cryptfs_hw
-#endif
+# QCOM HW crypto
+ifeq ($(TARGET_HW_DISK_ENCRYPTION),true)
+    TARGET_CRYPTFS_HW_PATH ?= vendor/qcom/opensource/cryptfs_hw
+endif
